c0ffd54a9f25a38fc3a683532c70b9b4284376dc
Derek <peter890701@gmail.com>
2024-09-24 21:56:07 +0800

    feat: add seriallog

--- src/EasyLotteryWasm/Program.cs
+++ src/EasyLotteryWasm/Program.cs
@@ -1,20 +1,35 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using EasyLotteryWasm;
-using EasyLotteryDomain.Services;
 using Blazorise;
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
+using EasyLotteryDomain.Services;
+using EasyLotteryWasm;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 var EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
+var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: true)
+                 .Build();
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Log.Logger = new LoggerConfiguration()
+                    //.ReadFrom.Configuration(configuration)
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithThreadName()
+                    .WriteTo.BrowserConsole()
+                    .WriteTo.Console()
+                    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddConfiguration(configuration);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
   // 注入 YouTubeServiceHelper 服務
@@ -27,5 +42,7 @@ builder.Services
     } )
     .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
+
+builder.Logging.AddSerilog(Log.Logger);
 
 await builder.Build().RunAsync();
